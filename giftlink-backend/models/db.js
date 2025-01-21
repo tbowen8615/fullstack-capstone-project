7854114fe5443c3bb79fa10398343068dc846ebc
tbowen8615 <tbowen8615@gmail.com>
2025-01-20 22:30:30 -0500

    back and front ends working

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -10,18 +10,13 @@ const dbName = "giftdb";
 
 async function connectToDatabase() {
     if (dbInstance){
-        return dbInstance;
+        return dbInstance
     };
 
     const client = new MongoClient(url);
 
-    // Task 1: Connect to MongoDB
     await client.connect();
-
-    // Task 2: Connect to database giftDB and store in variable dbInstance
     dbInstance = client.db(dbName);
-
-    // Task 3: Return database instance
     return dbInstance;
 }
 
