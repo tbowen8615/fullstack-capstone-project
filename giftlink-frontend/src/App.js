b635183000229b21cc4bd0352121e0dde3197a96
tbowen8615 <tbowen8615@gmail.com>
2025-01-22 01:07:46 -0500

    Create main.yml

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -11,7 +11,7 @@ import SearchPage from './components/SearchPage/SearchPage';
 import Profile from "./components/Profile/Profile.js";
 
 function App() {
-  const navigate = useNavigate();
+  //const navigate = useNavigate();
   return (
         <>
         <Navbar/>
